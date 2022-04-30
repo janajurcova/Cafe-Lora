6e7ca9dac5ba3cf7d7ae8cd7a4f96ba953fa0f64
Jana Jurčová <jj.jurcova@gmail.com>
2022-04-30 11:21:42 +0200

    hamburger menu navigation with toggle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
 
 console.log('funguju!');
+
+
+const burgerMenu =document.querySelector('#nav-btn')
+const navigation = document.querySelector('nav')
+const active = () => {
+    navigation.classList.toggle('nav-closed')
+}
+
+burgerMenu.addEventListener('click', active)
+
