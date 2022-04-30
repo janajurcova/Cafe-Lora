8321a8523073f358c2622ea08cfac5365323a85f
Jana Jurčová <jj.jurcova@gmail.com>
2022-04-30 12:20:04 +0200

    first drink order function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 console.log('funguju!');
 
-
+//hamburger menu
 const burgerMenu =document.querySelector('#nav-btn')
 const navigation = document.querySelector('nav')
 const active = () => {
@@ -14,4 +14,21 @@ burgerMenu.addEventListener('click', active)
 const navAll = document.querySelectorAll("nav a")
 navAll.forEach((item) => item.addEventListener('click', () => {
     navigation.classList.add('nav-closed')}))
+
+//objednat
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+let ordered = false;
+
+orderBtn.addEventListener('click', () => {
+ if ((ordered)) {
+        orderBtn.textContent = 'Objednat';
+        drinkCup.classList.remove('drink__cup--selected');
+        ordered = false;
+    } else {
+        orderBtn.textContent = 'Zrušit';
+        drinkCup.classList.add('drink__cup--selected');
+        ordered = true;
+    }
+});
 
