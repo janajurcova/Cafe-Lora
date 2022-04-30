dac67cf6a02a6f6234a6e3fab07ca0a51d33b51a
Jana Jurčová <jj.jurcova@gmail.com>
2022-04-30 11:56:05 +0200

    upgrade Hamburger menu

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const navigation = document.querySelector('nav')
 const active = () => {
     navigation.classList.toggle('nav-closed')
 }
-
 burgerMenu.addEventListener('click', active)
+
+
+const navAll = document.querySelectorAll("nav a")
+navAll.forEach((item) => item.addEventListener('click', () => {
+    navigation.classList.add('nav-closed')}))
 
